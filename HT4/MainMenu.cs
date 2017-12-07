bf9e6a0846fa08c1ec11555c24ef48c66f802e9a
9 Melody <minhdaik9x@gmail.com>
2017-12-08 00:40:14 +0700

    Demo1

--- HT4/MainMenu.cs
+++ HT4/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace HT4
 {
     public partial class MainMenu : Form
     {
+        string path = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\config.txt";
         public MainMenu()
         {
             InitializeComponent();
@@ -19,6 +21,7 @@ namespace HT4
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             btnTuVan.TabStop = false;
             btnTuVan.FlatStyle = FlatStyle.Flat;
             btnTuVan.FlatAppearance.BorderSize = 0;
@@ -37,6 +40,38 @@ namespace HT4
             btnThoat.TabStop = false;
             btnThoat.FlatStyle = FlatStyle.Flat;
             btnThoat.FlatAppearance.BorderSize = 0;
+            //check file config
+            try
+            {
+                if (File.Exists(path))
+                {
+                    string[] lines = System.IO.File.ReadAllLines(path);
+                    if (lines.Count() < 0)
+                    {
+                        Setting s = new Setting();
+                        s.Show();
+                        this.Hide();
+                    }
+                }
+                else
+                {
+                    FileStream fs = File.Create(path);
+                    StreamWriter wr = new StreamWriter(fs, Encoding.UTF8);
+                    wr.WriteLine("Địa chỉ máy chủ");
+                    wr.WriteLine("Tên cở sở dữ liệu");
+                    wr.WriteLine("Tài khoản");
+                    wr.WriteLine("Mật khẩu");
+                    wr.Flush();
+                    fs.Close();
+                    Setting s = new Setting();
+                    s.Show();
+                    this.Hide();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void btnTuVan_Click(object sender, EventArgs e)
